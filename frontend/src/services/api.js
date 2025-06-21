8faa87aeb7f7bae986c8464567bcd3de33184dbe
Jose Tadeu N de A Junior <teuipk@yahoo.com.br>
2025-06-21 12:33:37 -0300

    Atualiza baseURL da API para ambiente de produção

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://controle-estoque-xm8r.onrender.com/api',
 });
 
 export default api;
